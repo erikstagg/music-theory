9adbd7c5e62c4c0b04ac9ae2816f5853208c55fb
Erik Stagg <estagg@spigroup.com>
2019-10-09 17:15:53 -0400

    code formatting

--- js/musicTheory.js
+++ js/musicTheory.js
@@ -1,5 +1,9 @@
 var music_theory = {
+
+  // one octave of notes starting with C
   notes: ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"],
+
+  // scale note positions based on intervals
   scales: {
     major: [0, 2, 4, 5, 7, 9, 11, 0], // w-w-h-w-w-w-h
     natural_minor: [0, 2, 3, 5, 7, 8, 10, 0], // w-h-w-w-h-w-w
@@ -9,17 +13,19 @@ var music_theory = {
     minor_pentatonic: [0, 3, 5, 7, 10, 0], // Natural minor without degrees 2 and 6 (array positions 1 and 5)
   },
 
-  // get a specific scale by passing the base note and type of scale as arguements
+  /*
+    getScale
+    get a specific scale by passing the base note and type of scale as arguements
+  */
   getScale: function(note, type) {
     var notesShifted = this.notes.slice();
-    // shift the notes so that position 0 in the array is the first note in the scale
-    for (var i = 0; i < this.notes.indexOf(note); i++) {
+    for (var i = 0; i < this.notes.indexOf(note); i++) {     // shift the notes so that position 0 in the array is the first note in the scale
       var noteToShift = notesShifted.shift();
       notesShifted.push(noteToShift);
     }
+
     var scale = [];
-    // use the scale interval object to assign the notes of the scale
-    this.scales[type].forEach(notePos => {
+    this.scales[type].forEach(notePos => {      // use the scale interval object to assign the notes of the scale
       scale.push(notesShifted[notePos]);
     });
     return scale;
